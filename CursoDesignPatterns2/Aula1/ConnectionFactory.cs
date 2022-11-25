5fb26f0a401f4da620518931ba50491ecd8c7426
luanayasmim <luanayasmim11@outlook.com>
2022-11-25 14:57:34 -0300

    Factory

---
+++ CursoDesignPatterns2/Aula1/ConnectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace CursoDesignPatterns2.Aula1
+{
+    public class ConnectionFactory
+    {
+        public IDbConnection GetConnection()
+        {
+            IDbConnection connection = new SqlConnection();
+            connection.ConnectionString = "String de conexão";
+            //connection.ConnectionString = new ConfigReader().ReadConnectionString();
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
---
+++ CursoDesignPatterns2/Program.cs
@@ -0,0 +1,7 @@
+﻿using CursoDesignPatterns2.Aula1;
+using System.Data;
+
+IDbConnection connection = new ConnectionFactory().GetConnection();
+
+IDbCommand command = connection.CreateCommand();
+command.CommandText = "select * from table";
