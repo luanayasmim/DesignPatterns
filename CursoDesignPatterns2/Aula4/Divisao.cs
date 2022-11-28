1d6a1d6c0132b2e5b357d77e71ff2096e9f253f4
luanayasmim <luanayasmim11@outlook.com>
2022-11-28 11:24:02 -0300

    Interpreter

---
+++ CursoDesignPatterns2/Aula4/Divisao.cs
@@ -0,0 +1,21 @@
+﻿namespace CursoDesignPatterns2.Aula4
+{
+    public class Divisao : IExpressao
+    {
+        private IExpressao Esquerda;
+        private IExpressao Direita;
+
+        public Divisao(IExpressao esquerda, IExpressao direita)
+        {
+            Esquerda = esquerda;
+            Direita = direita;
+        }
+
+        public int Avalia()
+        {
+            int valorEsquerda = Esquerda.Avalia();
+            int valorDireita = Direita.Avalia();
+            return valorEsquerda / valorDireita;
+        }
+    }
+}
---
+++ CursoDesignPatterns2/Aula4/Multiplicacao.cs
@@ -0,0 +1,21 @@
+﻿namespace CursoDesignPatterns2.Aula4
+{
+    public class Multiplicacao : IExpressao
+    {
+        private IExpressao Esquerda;
+        private IExpressao Direita;
+
+        public Multiplicacao(IExpressao esquerda, IExpressao direita)
+        {
+            Esquerda = esquerda;
+            Direita = direita;
+        }
+
+        public int Avalia()
+        {
+            int valorEsquerda = Esquerda.Avalia();
+            int valorDireita = Direita.Avalia();
+            return valorDireita * valorDireita;
+        }
+    }
+}
