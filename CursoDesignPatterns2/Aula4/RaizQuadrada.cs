9ee5e398e2457a47c1d5dfb0874cbd3ee2c51ef4
luanayasmim <luanayasmim11@outlook.com>
2022-11-28 11:40:48 -0300

    Interpreter

---
+++ CursoDesignPatterns2/Aula4/RaizQuadrada.cs
@@ -0,0 +1,18 @@
+﻿namespace CursoDesignPatterns2.Aula4
+{
+    public class RaizQuadrada : IExpressao
+    {
+        private IExpressao Numero;
+
+        public RaizQuadrada(IExpressao numero)
+        {
+            Numero = numero;
+        }
+
+        public int Avalia()
+        {
+            int valorNumero = Numero.Avalia();
+            return (int)Math.Sqrt(valorNumero);
+        }
+    }
+}
